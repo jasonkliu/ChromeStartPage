3a912edb6944986929bbb7c434862e7d49b764d9
Jason Liu <sprintround@gmail.com>
2013-11-19 10:47:27 -0500

    added more quotes

--- quote.js
+++ quote.js
@@ -5,7 +5,7 @@
 
     function showNextQuote() {
 		//++quoteIndex;
-		var quoteIndex = Math.floor(Math.random() * 114);
+		var quoteIndex = Math.floor(Math.random() * 214);
         quotes.eq(quoteIndex % quotes.length)
             .fadeIn(2000)
             .delay(5000)
