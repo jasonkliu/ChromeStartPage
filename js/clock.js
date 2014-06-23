87f35eff6e0c83aebf0f6579091821243ae5da10
Jason Liu <jason@jasonkliu.com>
2014-06-22 22:33:50 -0700

    Rewrote clock_imperial.js in the new format

--- js/clock.js
+++ js/clock.js
@@ -13,53 +13,28 @@ function updateClock() {
 }
 
 // Update the clock in 12 hour time (such as 9:52 PM)
+function updateClockImperial() {
+    var now = new Date();
+    var minutes = now.getMinutes();
+    var date = now.getDate();
+    var month = now.getMonth() + 1;
+    var hours = now.getHours();
 
-function updateClockImperial()
-{
-    Date.getMinutesTwoDigits = function()
-    {
-        var min = now.getMinutes();
-        if (min < 10) return ("0" + min.toString());
-        else return min.toString();
+    var merid = "am"
+    if (hours > 12) {
+      hours -= 12;
+      merid = "pm"
+    } else if (hours == 0) {
+      hours = 12;
     }
 
-    Date.getHoursTwoDigits = function()
-    {
-        var hr = now.getHours();
-        var merid = "am";
-        if (hr >= 13) {
-           hr = hr-12;
-           merid = "pm";
-        }
-        if (hr == 0) {
-           hr = 12;
-        }
-        return [hr,merid];
-    }
-    Date.getDDMMYY = function()
-    {
-        var dd = now.getDate();
-        var mm = now.getMonth()+1;
-        var yyyy = now.getFullYear();
-        if (dd < 10) {
-          dd='0'+dd
-        }
-        if (mm < 10) {
-          mm='0'+mm
-        }
-        yyyy = mm+'/'+dd+'/'+yyyy;
-        return yyyy;
-    }
-    var now = new Date(),
+    time = hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + merid 
+      + ' ~ ' + (month < 10 ? '0' + month : month) + '/' 
+      + (date < 10 ? '0' + date : date) + '/' + now.getFullYear();
 
-    //Finding the hour and the merid(am/pm) from one function
-    hours = Date.getHoursTwoDigits();
-    var hrs = hours[0];
-    var merids = hours[1];
-
-    time = hrs + ':' + Date.getMinutesTwoDigits() + merids + ' ~ ' + Date.getDDMMYY();
     document.getElementById('time').innerHTML = ["", time].join('');
     setTimeout(updateClockImperial, 1000);
+
 }
 
 window.onload = updateClockImperial
