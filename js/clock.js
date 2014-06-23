40ccd6fb4b0646ad32ff474c238628f9adf22d96
Jason Liu <jason@jasonkliu.com>
2014-06-22 21:55:05 -0700

    Modified spacing in clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,34 +1,32 @@
-        function updateClock()
-        {
-            Date.getMinutesTwoDigits = function()
-            {
-                var min = now.getMinutes();
-                if (min < 10) return ("0" + min.toString());
-                else return min.toString();
-            }
-            Date.getHoursTwoDigits = function()
-            {
-                var hr = now.getHours();
-                return hr;
-            }
-            Date.getDDMMYY = function()
-            {
-                var dd = now.getDate();
-                var mm = now.getMonth()+1;
-                var yyyy = now.getFullYear();
-                if (dd < 10) {
-                  dd='0'+dd
-                }
-                if (mm < 10) {
-                  mm='0'+mm
-                }
-                yyyy = mm+'/'+dd+'/'+yyyy;
-                return yyyy;
-            }
-            var now = new Date(),
-            time = Date.getHoursTwoDigits() + ':' + Date.getMinutesTwoDigits() + ' ~ ' + Date.getDDMMYY();
-            document.getElementById('time').innerHTML = ["", time].join('');
-            setTimeout(updateClock, 1000);
+// Update the clock in 24 hour time (such as 21:52)
+function updateClock() {
+    Date.getMinutesTwoDigits = function() {
+        var min = now.getMinutes();
+        if (min < 10) return ("0" + min.toString());
+        else return min.toString();
+    }
+
+    Date.getHoursTwoDigits = function() {
+        var hr = now.getHours();
+        return hr;
+    }
+    Date.getDDMMYY = function() {
+        var dd = now.getDate();
+        var mm = now.getMonth() + 1;
+        var yyyy = now.getFullYear();
+        if (dd < 10) {
+          dd = '0' + dd
         }
+        if (mm < 10) {
+          mm = '0' + mm
+        }
+        yyyy = mm + '/' + dd + '/' + yyyy;
+        return yyyy;
+    }
+    var now = new Date(),
+    time = Date.getHoursTwoDigits() + ':' + Date.getMinutesTwoDigits() + ' ~ ' + Date.getDDMMYY();
+    document.getElementById('time').innerHTML = ["", time].join('');
+    setTimeout(updateClock, 1000);
+}
 
 window.onload=updateClock
